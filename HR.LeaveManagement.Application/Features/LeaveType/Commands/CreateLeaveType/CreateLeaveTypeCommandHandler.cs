28fd36121c20adedb4e0481928f282ba2325bb39
Mohamed Abdou <meldomany04@gmail.com>
2023-11-25 12:54:24 +0200

    CreateLeaveType CQRS Command Handler

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Presistence;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveType
+{
+    public class CreateLeaveTypeCommandHandler : IRequestHandler<CreateLeaveTypeCommand, int>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly Mapper _mapper;
+
+        public CreateLeaveTypeCommandHandler(ILeaveTypeRepository leaveTypeRepository, Mapper mapper)
+        {
+            this._leaveTypeRepository = leaveTypeRepository;
+            this._mapper = mapper;
+        }
+
+        public async Task<int> Handle(CreateLeaveTypeCommand request, CancellationToken cancellationToken)
+        {
+            var leaveTypeToCreate = _mapper.Map<Domain.LeaveType>(request);
+
+            await _leaveTypeRepository.CreateAsync(leaveTypeToCreate);
+
+            return leaveTypeToCreate.Id;
+        }
+    }
+}
