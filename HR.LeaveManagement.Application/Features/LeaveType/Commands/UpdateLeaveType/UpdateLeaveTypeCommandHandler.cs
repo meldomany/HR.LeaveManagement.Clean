c153b709804e31584733e45b1a4316648aaf9b2a
Mohamed Abdou <meldomany04@gmail.com>
2023-11-25 13:02:10 +0200

    UpdateLeaveType CQRS Command Handler

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/UpdateLeaveType/UpdateLeaveTypeCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Presistence;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.UpdateLeaveType
+{
+    public class UpdateLeaveTypeCommandHandler : IRequestHandler<UpdateLeaveTypeCommand, Unit>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly Mapper _mapper;
+
+        public UpdateLeaveTypeCommandHandler(ILeaveTypeRepository leaveTypeRepository, Mapper mapper)
+        {
+            this._leaveTypeRepository = leaveTypeRepository;
+            this._mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(UpdateLeaveTypeCommand request, CancellationToken cancellationToken)
+        {
+            var leaveTypeToUpdate = _mapper.Map<Domain.LeaveType>(request);
+
+            await _leaveTypeRepository.UpdateAsync(leaveTypeToUpdate);
+
+            return Unit.Value;
+        }
+    }
+}
