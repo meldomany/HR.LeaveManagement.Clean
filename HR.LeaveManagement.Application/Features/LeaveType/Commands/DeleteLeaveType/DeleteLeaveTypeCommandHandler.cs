5a60080370ae26f3de320eb7ffcbd3e9cd6d4b13
Mohamed Abdou <meldomany04@gmail.com>
2023-11-25 13:08:37 +0200

    Update and Delete LeaveType CQRS Command Handler

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/DeleteLeaveType/DeleteLeaveTypeCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Presistence;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.DeleteLeaveType
+{
+    public class DeleteLeaveTypeCommandHandler : IRequestHandler<DeleteLeaveTypeCommand, Unit>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly Mapper _mapper;
+
+        public DeleteLeaveTypeCommandHandler(ILeaveTypeRepository leaveTypeRepository, Mapper mapper)
+        {
+            this._leaveTypeRepository = leaveTypeRepository;
+            this._mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(DeleteLeaveTypeCommand request, CancellationToken cancellationToken)
+        {
+            var leaveTypeToDelete = await _leaveTypeRepository.GetByIdAsync(request.Id);
+
+            await _leaveTypeRepository.DeleteAsync(leaveTypeToDelete);
+
+            return Unit.Value;
+        }
+    }
+}
