5ba42cdcf1ac62f708fb7d92a4074b62add6b373
Mohamed Abdou <meldomany04@gmail.com>
2023-11-25 12:28:23 +0200

    GetLeaveTypes CQRS Handlig

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/GetLeaveTypeQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Presistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes
+{
+    public class GetLeaveTypeQueryHandler : IRequestHandler<GetLeaveTypesQuery, List<LeaveTypeDto>>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly Mapper _mapper;
+
+        public GetLeaveTypeQueryHandler(ILeaveTypeRepository leaveTypeRepository, Mapper mapper)
+        {
+            this._leaveTypeRepository = leaveTypeRepository;
+            this._mapper = mapper;
+        }
+
+        public async Task<List<LeaveTypeDto>> Handle(GetLeaveTypesQuery request, CancellationToken cancellationToken)
+        {
+            var leaveTypes = await _leaveTypeRepository.GetAsync();
+
+            var leaveTypesDto = _mapper.Map<List<LeaveTypeDto>>(leaveTypes);
+
+            return leaveTypesDto;
+        }
+    }
+}
