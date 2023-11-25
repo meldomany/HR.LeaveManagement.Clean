b5b8387d736b8015cfd4c0e51f8370be7c31d443
Mohamed Abdou <meldomany04@gmail.com>
2023-11-25 17:28:24 +0200

    Setup Persistence Project Inside Infrastructure Layer

---
+++ HR.LeaveManagement.Persistence/PersistenceServiceRegisteration.cs
@@ -0,0 +1,18 @@
+﻿using HR.LeaveManagement.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HR.LeaveManagement.Persistence
+{
+    public static class PersistenceServiceRegisteration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<HrDatabaseContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("HrDatabaseConnectionString"));
+            
+            return services;
+        }
+    }
+}
